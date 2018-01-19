5444a4f2b43e8c1d4c7e87aef55a4327d91b3b11
Thomas Huber <tomsemail2100@gmail.com>
2018-01-18 19:57:41 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include "frequencies.h"
+
+using namespace std;
+
+string text = "TO THE RED COUNTRY and part of the gray country of Oklahoma, the last rains came gently, and they did not cut the scarred earth. The plows crossed and recrossed the rivulet marks. The last rains lifted the corn quickly and scattered weed colonies and grass along the sides of the roads so that the gray country and the dark red country began to disappear under a green cover. In the last part of May the sky grew pale and the clouds that had hung in high puffs for so long in the spring were dissipated. The sun flared down on the growing corn day after day until a line of brown spread along the edge of each green bayonet. The clouds appeared, and went away, and in a while they did not try any more. The weeds grew darker green to protect themselves, and they did not spread any more. The surface of the earth crusted, a thin hard crust, and as the sky became pale, so the earth became pale, pink in the red country and white in the gray country. ";
+
+std::vector<char> chars;
+
+int main() {
+
+    std::cout << alphabet;
+
+    for (size_t i = 0; i < 55; ++i) {
+        cout << weights[i];
+    }
+    return 0;
+}
