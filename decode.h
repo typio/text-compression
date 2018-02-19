d1cb05beb9b5b9e3e65db2ccf6172febec5c0d28
Thomas Huber <tomsemail2100@gmail.com>
2018-02-18 19:25:10 -0700

    fixed find char from code functionality

--- decode.h
+++ decode.h
@@ -6,8 +6,21 @@
 #include <vector>
 
 char getChar(string code, string codes) {
-    cout << codes[codes.find(code) - 3];
-    return codes[codes.find(code) - 3];
+    int pos = codes.find(code);
+    int findStart = 0;
+    bool found = false;
+    while (!found) {
+        pos = codes.find(code, findStart);
+        char c = codes[pos - 2];
+        if (c == '>' || c == '<' || c == '1' || c == '0') {
+            findStart++;
+        } else {
+            found = true;
+        }
+    }
+    // this cout is just for fun
+    cout << codes[pos - 2];
+    return codes[pos - 2];
 }
 
 void writeDecompressed(string compressedPath, string freqsPath) {
@@ -32,7 +45,7 @@ void writeDecompressed(string compressedPath, string freqsPath) {
     istream_iterator<string> begin(ss);
     istream_iterator<string> end;
     vector<string> codes(begin, end);
-    copy(codes.begin(), codes.end(), ostream_iterator<string>(cout));
+    copy(codes.begin(), codes.end(), ostream_iterator<string>(cout, " "));
 
     for (size_t i = 0; i < codes.size(); i++) {
         decompressed << getChar(codes.at(i), freqsStr);
